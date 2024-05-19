d8eabd5828e9226eb1800275e0608691da439d39
Mohit Singh Shah <mohitshah3.ms@gmail.com>
2024-05-19 20:25:09 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,7 @@
+
+
+
+
+const heading = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"Hello Mohit"),React.createElement("h2",{},"Hello Sopmesh")]))
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
